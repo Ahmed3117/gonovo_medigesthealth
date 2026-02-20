889a1135026c361073c721e0ae017dc84a75f423
Ahmed3117 <ahmedibra3117@gmail.com>
2026-02-20 15:11:54 +0200

    Auto-create superuser from env vars in build.sh

--- build.sh
+++ build.sh
@@ -8,3 +8,8 @@ pip install -r requirements.txt
 cd src
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser from env vars (only if it doesn't already exist)
+if [ -n "$DJANGO_SUPERUSER_EMAIL" ]; then
+    python manage.py createsuperuser --noinput 2>/dev/null || echo "Superuser already exists."
+fi
