adc61ab1350fffd275e5a76f561b91ceac521ec8
Ahmed3117 <ahmedibra3117@gmail.com>
2026-02-21 17:36:56 +0200

    Add seed_data to build.sh for auto-seeding on deploy

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ pip install -r requirements.txt
 cd src
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py seed_data
 
 # Create superuser from env vars (only if it doesn't already exist)
 if [ -n "$DJANGO_SUPERUSER_EMAIL" ]; then
